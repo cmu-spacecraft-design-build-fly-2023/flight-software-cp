179436de8ae684e74efecf7e154787b79f2d8f87
Harry Rosmann <47433061+harryrosmann@users.noreply.github.com>
2024-01-29 18:51:11 -0500

    Initial commit

---
+++ mainboard-v05/lib/opt4001.py
@@ -0,0 +1,13 @@
+"""
+`opt4001`
+====================================================
+
+CircuitPython driver for the OPT4001 lux sun sensor
+https://www.ti.com/lit/ds/symlink/opt4001.pdf?ts=1706572218523&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FOPT4001%253Futm_source%253Dgoogle%2526utm_medium%253Dcpc%2526utm_campaign%253Dasc-sens-null-44700045336317194_prodfolderdynamic-cpc-pf-google-wwe_int%2526utm_content%253Dprodfolddynamic%2526ds_k%253DDYNAMIC%2BSEARCH%2BADS%2526DCM%253Dyes%2526gad_source%253D1%2526gclid%253DCjwKCAiAtt2tBhBDEiwALZuhAA1thLbavDhNeslk1fNpOmPKjd3I25YaUHs_5qm
+
+* Author(s): 
+
+Implementation Notes
+--------------------
+
+"""
